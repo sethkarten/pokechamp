b48b141cda56b6cfab8e2d904f9ce1593f8f65a3
Daniel10720 <dl4247@princeton.edu>
2025-07-10 15:53:40 -0400

    testing preexisting vgc capabilities

--- human_agent_1v1.py
+++ human_agent_1v1.py
@@ -8,7 +8,7 @@ from poke_env.player.team_util import get_llm_player, load_random_team
 parser = argparse.ArgumentParser()
 parser.add_argument("--temperature", type=float, default=0.3)
 parser.add_argument("--prompt_algo", default="minimax", choices=prompt_algos)
-parser.add_argument("--battle_format", default="gen9ou", choices=["gen8randombattle", "gen8ou", "gen9ou", "gen9randombattle"])
+parser.add_argument("--battle_format", default="gen9ou", choices=["gen8randombattle", "gen8ou", "gen9ou", "gen9randombattle", "gen9vgc2025regi"])
 parser.add_argument("--backend", type=str, default="gpt-4o", choices=["gpt-4o-mini", "gpt-4o", "gpt-4o-2024-05-13", "llama", 'None'])
 parser.add_argument("--log_dir", type=str, default="./battle_log/ladder")
 parser.add_argument("--device", type=int, default=0)
@@ -24,10 +24,13 @@ async def main():
                             PNUMBER1=PNUMBER1,
                             battle_format=args.battle_format)
     if not 'random' in args.battle_format:
-        opponent.update_team(load_random_team())                      
+        if 'vgc' in args.battle_format:
+            opponent.update_team(load_random_team(vgc=True))
+        else: 
+            opponent.update_team(load_random_team())                      
     
     # Playing 5 games on local
-    for i in tqdm(range(5)):
+    for i in tqdm(range(1)):
         await opponent.ladder(1)
         if not 'random' in args.battle_format:
             opponent.update_team(load_random_team())
